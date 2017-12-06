a12f2dc5e269c61573ebb5bc56502f732ce8e136
jpdias <jpdias@live.com.pt>
2017-12-06 00:59:48 +0000

    fixs and eslint

---
+++ main.js
@@ -0,0 +1,6 @@
+var gazebojs = require('gazebojs')
+var gazebo = new gazebojs.Gazebo()
+
+//gazebo.publish('gazebo.msgs.WorldControl', '~/world_control', { pause: true});
+
+//gazebo.subscribe('gazebo.msgs.WorldStatistics',  '~/world_stats', console.log);
