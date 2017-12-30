6f8dae35d3461fdb4129e450c89f9fee066b18b9
jpdias <jpdias@live.com.pt>
2017-12-30 20:15:33 +0000

    listen to cameras

--- script.py
+++ script.py
@@ -1,25 +1,52 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 import rospy
-from std_msgs.msg import String
 from geometry_msgs.msg import Twist
+from std_msgs.msg import MultiArrayLayout, MultiArrayDimension, Float64MultiArray, Bool, String
+from sensor_msgs.msg import Image
 
-def callback(msg):
+import configparser
+
+TOPICSTOLISTEN = []
+
+
+def setup():
+    config = configparser.ConfigParser()
+    # config.sections()
+    config.read('config.ini')
+    #print config.get('DEFAULT', 'TopicsListen')
+    TOPICSTOLISTEN.extend(config.get("DEFAULT", "TopicsListen").split(','))
+    print "Configs Load Done"
+
+
+def callback_cmd_vel(msg):
     rospy.loginfo("Received a /cmd_vel message!")
-    rospy.loginfo("Linear Components: [%f, %f, %f]"%(msg.linear.x, msg.linear.y, msg.linear.z))
-    rospy.loginfo("Angular Components: [%f, %f, %f]"%(msg.angular.x, msg.angular.y, msg.angular.z))
+    rospy.loginfo("Linear Components: [%f, %f, %f]" % (
+        msg.linear.x, msg.linear.y, msg.linear.z))
+    rospy.loginfo("Angular Components: [%f, %f, %f]" % (
+        msg.angular.x, msg.angular.y, msg.angular.z))
     pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
-    
-    rate = rospy.Rate(10) # 10hz  
-    pub.publish(msg)  
 
+    rate = rospy.Rate(10)  # 10hz
+    pub.publish(msg)
 
-def listener():
+def callback_cameras(msg):
+    rospy.loginfo("camera:: "+str(msg))
 
-    rospy.init_node('listener', anonymous=True)
+def listener_cmd_vel():
+    print "listenning to cmd_vel"
+    rospy.Subscriber("cmd_vel_fake", Twist, callback_cmd_vel)
 
-    rospy.Subscriber("cmd_vel_fake", Twist, callback)
-
-    rospy.spin()
+def listener_cameras():
+    print "listenning to cameras"
+    rospy.Subscriber("conde_camera_tracking_right/image_raw", Image, callback_cameras)
+    rospy.Subscriber("conde_camera_tracking_left/image_raw", Image, callback_cameras)
 
 if __name__ == '__main__':
-    listener()
+    setup()
+    rospy.init_node('listener', anonymous=True)
+    if "cmd_vel" in TOPICSTOLISTEN:
+        listener_cmd_vel()
+    if "cameras" in TOPICSTOLISTEN:
+        listener_cameras()
+    rospy.spin()
+
