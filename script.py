17023a17849d21b43b38f2060805265189634a60
jpdias <jpdias@live.com.pt>
2018-01-02 00:44:01 +0000

    fix

--- script.py
+++ script.py
@@ -8,6 +8,8 @@ import configparser
 import threading
 import time
 import message_filters
+import random
+import numpy
 
 TOPICSTOLISTEN = []
 
@@ -15,33 +17,35 @@ CAMERAQUEUE = Queue.Queue()
 
 CONFIG = configparser.ConfigParser()
 
-
-def camera_queue_pub(msg, topic):
-    if topic == "right":
-        pub = rospy.Publisher(
-            "conde_camera_tracking_right/image_raw_fake", Image, queue_size=10)
-    elif topic == "left":
-        pub = rospy.Publisher(
-            "conde_camera_tracking_left/image_raw_fake", Image, queue_size=10)
-    rate = rospy.Rate(10)  # 10hz
-    pub.publish(msg)
-
-
 def worker():
     while True:
         item = CAMERAQUEUE.get()
         item_right = item[1]
         item_left = item[0]
-        if CONFIG.getint("DEFAULT", "Time"):
-            time.sleep(CONFIG.getint("DEFAULT", "Time"))
-        camera_queue_pub(item_right, "right")
-        camera_queue_pub(item_left, "left")
+        #if CONFIG.getint("SLOW", "time"):
+        #    time.sleep(CONFIG.getint("SLOW", "time") / 1000.0)
+        if CONFIG.getint("RANDOM", "seed"):
+            np_arr_right = numpy.fromstring(item_right.data, numpy.uint8)
+            print np_arr_right
+            rRandom = numpy.array(random.shuffle(np_arr_right.tolist()))
+            item_right.data = rRandom.tostring()
+            print numpy.fromstring(item_right.data, numpy.uint8)
+
+            np_arr_left = numpy.fromstring(item_left.data, numpy.uint8)
+            print np_arr_left
+            lRandom = numpy.array(random.shuffle(np_arr_left.tolist()))
+            item_left.data = lRandom.tostring()
+            print numpy.fromstring(item_left.data, numpy.uint8)
+        pub_right = rospy.Publisher(
+            "conde_camera_tracking_right/image_raw_fake", Image, queue_size=10)
+        pub_left = rospy.Publisher(
+            "conde_camera_tracking_left/image_raw_fake", Image, queue_size=10)
+        #camera_queue_pub(item_right, "right")
+        #camera_queue_pub(item_left, "left")
+        rospy.Rate(10)  # 10hz
+        pub_right.publish(item_right)
+        pub_left.publish(item_left)
         CAMERAQUEUE.task_done()
-
-
-T = threading.Thread(target=worker)
-T.daemon = True
-T.start()
 
 
 def setup():
@@ -67,7 +71,7 @@ def callback_cmd_vel(msg):
 def callback_cameras(msg_left, msg_right):
     t = msg_left, msg_right
     CAMERAQUEUE.put(t)
-    rospy.loginfo("left:: " + str(msg_left) + "\nright:: " + str(msg_left))
+    #rospy.loginfo("left:: " + str(msg_left) + "\nright:: " + str(msg_left))
 
 
 def listener_cmd_vel():
@@ -78,10 +82,8 @@ def listener_cmd_vel():
 def listener_cameras():
     print "listenning to cameras"
 
-    image_right =  rospy.Subscriber("conde_camera_tracking_right/image_raw",
-                     Image)
-    image_left = rospy.Subscriber("conde_camera_tracking_left/image_raw",
-                     Image)
+    image_right =  message_filters.Subscriber("conde_camera_tracking_right/image_raw", Image)
+    image_left = message_filters.Subscriber("conde_camera_tracking_left/image_raw", Image)
 
     ts = message_filters.TimeSynchronizer([image_left, image_right], 40)
     ts.registerCallback(callback_cameras)
@@ -93,4 +95,7 @@ if __name__ == '__main__':
         listener_cmd_vel()
     if "cameras" in TOPICSTOLISTEN:
         listener_cameras()
+    T = threading.Thread(target=worker)
+    T.daemon = True
+    T.start()
     rospy.spin()
